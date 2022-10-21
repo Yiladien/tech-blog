f4f4e7bd8ee36bda90cc9a4d323ae72b5790d215
yiladien@hotmail.com <yiladien@hotmail.com>
2022-10-21 04:26:00 -0500

    password box updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment, Vote } = require("../models");
+const { Post, User, Comment } = require("../models");
 
 // get all posts for homepage
 router.get("/", (req, res) => {
-  Post.findAll({})
+  Post.findAll({
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-
+      console.log(posts);
       res.render("homepage", {
         posts,
         loggedIn: req.session.loggedIn,
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,53 +1,72 @@
-async function loginFormHandler(event) {
-  event.preventDefault();
+$(document).ready(function () {
+  $("#show_hide_password a").on("click", function (event) {
+    event.preventDefault();
+    if ($("#show_hide_password input").attr("type") == "text") {
+      $("#show_hide_password input").attr("type", "password");
+      $("#show_hide_password i").addClass("fa-eye-slash");
+      $("#show_hide_password i").removeClass("fa-eye");
+    } else if ($("#show_hide_password input").attr("type") == "password") {
+      $("#show_hide_password input").attr("type", "text");
+      $("#show_hide_password i").removeClass("fa-eye-slash");
+      $("#show_hide_password i").addClass("fa-eye");
+    }
+  });
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  async function loginFormHandler(event) {
+    event.preventDefault();
 
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
 
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
+    if (email && password) {
+      const response = await fetch("/api/users/login", {
+        method: "post",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/dashboard/");
+      } else {
+        alert(response.statusText);
+      }
     }
   }
-}
 
-async function signupFormHandler(event) {
-  event.preventDefault();
+  async function signupFormHandler(event) {
+    event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector("#username-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+    if (username && email && password) {
+      const response = await fetch("/api/users", {
+        method: "post",
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
 
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
+      if (response.ok) {
+        document.location.replace("/dashboard/");
+      } else {
+        alert(response.statusText);
+      }
     }
   }
-}
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  document
+    .querySelector(".login-form")
+    .addEventListener("submit", loginFormHandler);
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  document
+    .querySelector(".signup-form")
+    .addEventListener("submit", signupFormHandler);
+});
